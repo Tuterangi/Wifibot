3fffbe2327cf30c4439c7d18e1401bdc2ce07e17
LanclumeC <120129608+LanclumeC@users.noreply.github.com>
2023-05-23 09:37:42 +0200

    modification appuis touche en pressage touches
    Pour mieux piloter le robot, on change le moyen de déplacement à l'aide des touches du clavier. Au lieu de le déplacer lorsque l'on appuis sur ZQSD et l'arrête quand on appuis sur un autre touche, on le déplace quand on reste appuyé sur ZQSD et on l'arrête si la touche est relâché.

--- mainwindow.h
+++ mainwindow.h
@@ -18,15 +18,19 @@ public:
     ~MainWindow();
 
 private slots:
+    //création des boutons pour avancer
     void on_pushButton_2_pressed();
     void on_pushButton_2_released();
 
+    //création des boutons pour tourner à droite
     void on_pushButton_3_pressed();
     void on_pushButton_3_released();
 
+    //création des boutons pour reculer
     void on_pushButton_4_pressed();
     void on_pushButton_4_released();
 
+    //création des boutons pour tourner à gauche
     void on_pushButton_pressed();
     void on_pushButton_released();
 
@@ -35,29 +39,45 @@ private:
     MyRobot myrobot;
 
 protected:
-    void keyPressEvent(QKeyEvent *event) override
+    void keyPressEvent(QKeyEvent *event) override //Réécriture de la fonction keyPressEvent pour nos besoins
     {
+        if(event->key() == Qt::Key_Z)//Touche Z du clavier pressé
+        {
+           myrobot.move_forward();//Le robot avance
+        }
+        else if(event->key() == Qt::Key_D)//Si touche D du clavier pressé
+        {
+            myrobot.turn_right();//Le robot tourne à droite
+        }
+        else if(event->key() == Qt::Key_S)//Si touche S du clavier pressé
+        {
+            myrobot.move_backward();//Le robot recule
+        }
+        else if(event->key() == Qt::Key_Q)//Si touche Q du clavier pressé
+        {
+            myrobot.turn_left();//Le robot tourne à gauche
+        }
+        QMainWindow::keyPressEvent(event);
+    }
+    void keyReleaseEvent(QKeyEvent *event) override //Réécriture de la fonction keyReleaseEvent pour nos besoins
+    { //si les touches ZQSD sont relâché, le robot s'arrête là où il est
         if(event->key() == Qt::Key_Z)
-        {
-            myrobot.move_forward();
-        }
-        else if(event->key() == Qt::Key_D)
-        {
-            myrobot.turn_right();
-        }
-        else if(event->key() == Qt::Key_S)
-        {
-            myrobot.move_backward();
-        }
-        else if(event->key() == Qt::Key_Q)
-        {
-            myrobot.turn_left();
-        }
-        else
         {
             myrobot.stop();
         }
-
+        else if(event->key() == Qt::Key_D)
+        {
+            myrobot.stop();
+        }
+        else if(event->key() == Qt::Key_S)
+        {
+            myrobot.stop();
+        }
+        else if(event->key() == Qt::Key_Q)
+        {
+            myrobot.stop();
+        }
+        QMainWindow::keyReleaseEvent(event);
     }
 
 };
